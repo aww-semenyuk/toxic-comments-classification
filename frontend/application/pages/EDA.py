0ac0d1ace67bafa4084b01c98486d5a640edc352
Inre Leppik <inreleppik@MacBook-Pro-Inre-2.local>
2024-12-27 20:03:50 +0300

    Внесены некоторые исправления и добавлены файлы requirments.txt

--- frontend/application/pages/EDA.py
+++ frontend/application/pages/EDA.py
@@ -21,6 +21,12 @@ import base64
 import io
 import random
 
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('universal_tagset')
+
 
   
 
