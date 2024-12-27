725daab56eea5bb576167452883b056184abe912
Лев Халяпин <lev.halyapin@yandex.ru>
2024-12-27 19:11:22 +0300

    Logging & Refactoring

--- SERVICE/backend/main.py
+++ SERVICE/backend/main.py
@@ -3,12 +3,13 @@ from concurrent.futures import ProcessPoolExecutor
 from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, ConfigDict
 
 from api.v1.background_tasks import router as background_tasks_router
 from api.v1.trainer import router as trainer_router
 from settings.app_config import AppConfig
+from settings.logger_config import logger
 
 
 @asynccontextmanager
@@ -21,6 +22,8 @@ async def lifespan(app: FastAPI):
     app.state.process_executor = ProcessPoolExecutor(
         max_workers=app_config.cores_cnt - 1
     )
+
+    logger.info("Application started")
     yield
     app.state.process_executor.shutdown(wait=True)
 
@@ -34,6 +37,15 @@ app = FastAPI(
     openapi_url="/api/openapi.json",
     lifespan=lifespan
 )
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    method = request.method
+    url = str(request.url)
+    http_version = request.scope.get("http_version", "1.1")
+    response = await call_next(request)
+    logger.info(f'"{method} {url} HTTP/{http_version}" {response.status_code}')
+    return response
 
 
 class StatusResponse(BaseModel):
