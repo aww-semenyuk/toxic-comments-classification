4b7b5091aa966365c9cf665bb82b8053cf3f035a
vlad <zippo585@gmail.com>
2024-12-30 12:00:05 +0500

    Rebasing

--- SERVICE/backend/serializers/trainer.py
+++ SERVICE/backend/serializers/trainer.py
@@ -1,61 +1,80 @@
+import re
 from enum import Enum
 from pathlib import Path
 
-from pydantic import BaseModel
-from typing import Literal
+from pydantic import BaseModel, Field, field_validator
+
 
 class MessageResponse(BaseModel):
+    """Pydantic model for the message response."""
     message: str
 
-class ModelListResponse(BaseModel):
-    models: list[dict]
 
 class MLModelType(str, Enum):
-    LogisticRegression = "logistic_regression"
-    MultinomialNB = "multinomial_naive_bayes"
-    LinearSVC = "linear_svc"
+    """Enum for model type."""
+    logistic_regression = "logistic_regression"
+    multinomial_nb = "multinomial_naive_bayes"
+    linear_svc = "linear_svc"
+
 
 class VectorizerType(str, Enum):
-    CountVectorizer = "bag_of_words"
-    TfidfVectorizer = "tf_idf"
+    """Enum for vectorizer type."""
+    count_vectorizer = "bag_of_words"
+    tfidf_vectorizer = "tf_idf"
+
 
 class MLModelConfig(BaseModel):
+    """Pydantic model for the model config."""
     id: str
-    custom_tokenizer: Literal["spacy_lemma", None] = "spacy_lemma"
-    vectorizer_type: VectorizerType = VectorizerType.CountVectorizer
-    vectorizer_params: dict = {}
-    ml_model_type: MLModelType = MLModelType.LogisticRegression
-    ml_model_params: dict = {}
+    spacy_lemma_tokenizer: bool = False
+    vectorizer_type: VectorizerType
+    vectorizer_params: dict
+    ml_model_type: MLModelType
+    ml_model_params: dict
+
+    @field_validator("id")
+    def validate_id(cls, v):
+        """Validator for the ID field."""
+        if not bool(re.compile(r"^[a-z0-9_]+(?:[-_][a-z0-9_]+)*$").match(v)):
+            raise ValueError(
+                "ID модели может состоять только из строчных латинских букв, "
+                "цифр,  дефисов, и знаков подчеркивания"
+            )
+        return v
+
 
 class LoadRequest(BaseModel):
+    """Pydantic model for the load request."""
     id: str
 
-    model_config = {"json_schema_extra": {"example": {"id": "model_1"}}}
-
-class GetStatusResponse(BaseModel):
-    status: str
 
 class UnloadRequest(LoadRequest):
+    """Pydantic model for the unload request."""
     pass
 
-class PredictRequest(BaseModel):
-    id: str
-    X: list[str]
 
-    model_config = {
-        "json_schema_extra": {"example": {
-            "id": "model_1",
-            "X": ["text text 1", "text text 2"]}}}
+class PredictRequest(BaseModel):
+    """Pydantic model for the predict request."""
+    X: list[str] = Field(
+        description="Кавычки в тексте необходимо экранировать"
+    )
+
 
 class PredictResponse(BaseModel):
-    predictions: list
+    """Pydantic model for the predict response."""
+    predictions: list[int]
 
 
 class MLModelInListResponse(BaseModel):
+    """Pydantic model for the model in list response."""
     id: str
     type: MLModelType
     is_trained: bool = False
+    is_loaded: bool = False
+    model_params: dict = Field(default_factory=dict)
+    vectorizer_params: dict = Field(default_factory=dict)
 
 
 class MLModel(MLModelInListResponse):
+    """Pydantic model for the model."""
     saved_model_file_path: Path | None = None
--- SERVICE/backend/services/utils/trainer.py
+++ SERVICE/backend/services/utils/trainer.py
@@ -1,24 +1,36 @@
+from pathlib import Path
+import unicodedata
+
 import cloudpickle
+import pandas as pd
 
 import spacy
 import nltk
-import unicodedata
 
-from sklearn.base import BaseEstimator, TransformerMixin, clone
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-from serializers.trainer import MLModelType, VectorizerType, FitConfig
+from serializers.trainer import MLModelType, VectorizerType, MLModelConfig
+from settings.app_config import MODELS_DIR
+
+AVAILABLE_ESTIMATORS = {
+    MLModelType.logistic_regression: LogisticRegression,
+    MLModelType.multinomial_nb: MultinomialNB,
+    MLModelType.linear_svc: LinearSVC
+}
+AVAILABLE_VECTORIZERS = {
+    VectorizerType.count_vectorizer: CountVectorizer,
+    VectorizerType.tfidf_vectorizer: TfidfVectorizer
+}
 
 
 class FunctionWrapper:
-    """
-    Wrapper to support serialization in multiprocessing
-    credit: https://stackoverflow.com/a/75720040
-    """
+    """Wrapper to support serialization in multiprocessing."""
+
     def __init__(self, fn):
         self.fn_ser = cloudpickle.dumps(fn)
 
@@ -26,27 +38,9 @@ class FunctionWrapper:
         fn = cloudpickle.loads(self.fn_ser)
         return fn(*args, **kwargs)
 
-AVAILABLE_ESTIMATORS = {
-    MLModelType.LogisticRegression: LogisticRegression(), 
-    MLModelType.MultinomialNB: MultinomialNB(),
-    MLModelType.LinearSVC: LinearSVC()
-}
-
-AVAILABLE_VECTORIZERS = {
-    VectorizerType.CountVectorizer: CountVectorizer(), 
-    VectorizerType.TfidfVectorizer: TfidfVectorizer()
-}
-
-VEC_PARAMS_FOR_SPACY_TOKENIZED_TEXT = {
-    'tokenizer': FunctionWrapper(lambda x: x.split('\t')),
-    'strip_accents': None,
-    'lowercase': False,
-    'preprocessor': None,
-    'stop_words': None,
-    'token_pattern': None
-}
 
 class SpacyTokenizer(BaseEstimator, TransformerMixin):
+    """Tokenizer using Spacy."""
     nlp = spacy.load('en_core_web_sm')
     stopwords = set(nltk.corpus.stopwords.words('english'))
 
@@ -57,32 +51,96 @@ class SpacyTokenizer(BaseEstimator, TransformerMixin):
 
     @staticmethod
     def normalize_text(doc):
-        return unicodedata.normalize('NFKD', doc).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-    
+        """Normalize the text."""
+        return unicodedata.normalize(
+            'NFKD', doc
+        ).encode(
+            'ascii',
+            'ignore'
+        ).decode(
+            'utf-8',
+            'ignore'
+        )
+
     def fit(self, X, y=None):
+        """Fit the tokenizer."""
         return self
-    
+
     def transform(self, X):
+        """Transform the input data."""
         results = []
 
-        pipe = self.nlp.pipe(map(self.normalize_text, X), disable=['ner', 'parser'], batch_size=self.batch_size, n_process=self.n_process)
+        pipe = self.nlp.pipe(
+            map(self.normalize_text, X),
+            disable=['ner', 'parser'],
+            batch_size=self.batch_size,
+            n_process=self.n_process
+        )
 
         for doc in pipe:
-            results.append(self.sep.join([token.lemma_.lower() for token in doc if not (token.lemma_.lower() in self.stopwords or token.is_space or token.is_punct)]))
+            results.append(self.sep.join([
+                token.lemma_.lower() for token in doc
+                if not (
+                    token.lemma_.lower() in self.stopwords
+                    or token.is_space
+                    or token.is_punct
+                )
+            ]))
 
         return results
 
-def make_pipeline_from_config(config: FitConfig):
-    estimator = clone(AVAILABLE_ESTIMATORS[config.ml_model_type]).set_params(**config.ml_model_params)
-    vec = clone(AVAILABLE_VECTORIZERS[config.vectorizer_type]).set_params(**config.vectorizer_params)
 
-    if config.custom_tokenizer == "spacy_lemma":
-        vec = vec.set_params(**VEC_PARAMS_FOR_SPACY_TOKENIZED_TEXT)
-        pipe = Pipeline(steps=[('tok', SpacyTokenizer()), 
-                               ('vec', vec), 
-                               ('estimator', estimator)])
+def make_pipeline_from_config(
+    config: MLModelConfig
+) -> tuple[Pipeline, dict, dict]:
+    """Make a pipeline from the configuration."""
+    estimator = AVAILABLE_ESTIMATORS[config.ml_model_type]().set_params(
+        **config.ml_model_params
+    )
+    vectorizer = AVAILABLE_VECTORIZERS[config.vectorizer_type]().set_params(
+        **config.vectorizer_params
+    )
+
+    if config.spacy_lemma_tokenizer:
+        vectorizer = vectorizer.set_params(
+            tokenizer=FunctionWrapper(lambda x: x.split('\t')),
+            strip_accents=None,
+            lowercase=False,
+            preprocessor=None,
+            stop_words=None,
+            token_pattern=None
+        )
+        pipe = Pipeline(steps=[
+            ('tok', SpacyTokenizer()),
+            ('vec', vectorizer),
+            ('estimator', estimator)
+        ])
     else:
-        pipe = Pipeline(steps=[('vec', vec), 
-                               ('estimator', estimator)])
+        pipe = Pipeline(steps=[
+            ('vec', vectorizer),
+            ('estimator', estimator)
+        ])
 
-    return pipe
+    return pipe, estimator.get_params(), vectorizer.get_params()
+
+
+def train_and_save_model_task(
+    model_config: MLModelConfig,
+    fit_dataset: pd.DataFrame
+) -> tuple[Path, dict, dict]:
+    """Train and save a model."""
+    model_id = model_config.id
+
+    X = fit_dataset["comment_text"]
+    y = fit_dataset["toxic"]
+
+    pipe, model_params, vectorizer_params = make_pipeline_from_config(
+        model_config
+    )
+    pipe.fit(X, y)
+
+    model_file_path = MODELS_DIR / f"{model_id}.cloudpickle"
+    with model_file_path.open('wb') as file:
+        cloudpickle.dump(pipe, file)
+
+    return model_file_path, model_params, vectorizer_params
