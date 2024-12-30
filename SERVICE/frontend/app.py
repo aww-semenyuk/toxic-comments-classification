ee54f1b7ed510dc10c51ea45578121cd1a010a23
Alexander Semenyuk <aww.semenyuk@gmail.com>
2024-12-30 16:03:17 +0300

    rename files

--- SERVICE/frontend/app.py
+++ SERVICE/frontend/app.py
@@ -1,38 +1,38 @@
 import streamlit as st
 
 home_page = st.Page(
-    'pages/file_upload.py',
+    'app_pages/file_upload.py',
     title='Homepage/data upload',
     icon=":material/upload_file:",
     default=True
 )
 eda_page = st.Page(
-    'pages/eda.py',
+    'app_pages/eda.py',
     title='EDA',
     icon=":material/equalizer:"
 )
 train_page = st.Page(
-    'pages/train_models.py',
+    'app_pages/train_models.py',
     title="Train models",
     icon=":material/cycle:"
 )
 manage_page = st.Page(
-    'pages/manage_models.py',
+    'app_pages/manage_models.py',
     title="Manage models",
     icon=":material/settings:"
 )
 bg_page = st.Page(
-    'pages/bg_tasks.py',
+    'app_pages/bg_tasks.py',
     title="Monitor tasks",
     icon=":material/monitoring:"
 )
 predict_page = st.Page(
-    'pages/predict.py',
+    'app_pages/predict.py',
     title="Predict",
     icon=":material/network_node:"
 )
 scores_page = st.Page(
-    'pages/predict_scores.py',
+    'app_pages/predict_scores.py',
     title="Compare models by ROC",
     icon=":material/compare_arrows:"
 )
